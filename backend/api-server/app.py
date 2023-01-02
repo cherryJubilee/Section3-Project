6df21bee29357d1b4fde3710b769910e6720e1bf
cherryJubilee <112672507+cherryJubilee@users.noreply.github.com>
2023-01-02 21:46:42 +0900

    add api server response redirection

--- backend/api-server/app.py
+++ backend/api-server/app.py
@@ -7,6 +7,12 @@ app = Flask(__name__)
 url = "http://hyewon-section3.s3-website.ap-northeast-2.amazonaws.com"
 
 
+@app.after_request
+def add_header(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
+
+
 @app.route('/', methods=['GET', 'POST'])
 def main():
     if request.method == 'POST':
@@ -20,7 +26,7 @@ def predict_mbti(answer_data):
         model = pickle.load(pickle_file)
     X_test = [list(answer_data)]
     y_pred = model.predict(X_test)
-    return y_pred
+    return y_pred.tolist()[0]
 
 
 @app.route('/survey/<data>', methods=['GET', 'POST'])
@@ -39,20 +45,20 @@ def submit_survey(data):
     # MBTI 예측
     try:
         mbti = predict_mbti(data[:8])
-        print(f'mbti: {mbti}')
+        print(f'mbti: {mbti}, type({type(mbti)})')
     except Exception as e:
         print(f'PREDICT FAIL: {e}')
 
-    if mbti is 0:
-        return redirect(f"{url}/0.html", code=302)
-    elif mbti is 1:
-        return redirect(f"{url}/1.html", code=302)
-    elif mbti is 2:
-        return redirect(f"{url}/2.html", code=302)
-    elif mbti is 3:
-        return redirect(f"{url}/3.html", code=302)
+    if mbti == 0:
+        return redirect(f"{url}/0.html", code=301)
+    elif mbti == 1:
+        return redirect(f"{url}/1.html", code=301)
+    elif mbti == 2:
+        return redirect(f"{url}/2.html", code=301)
+    elif mbti == 3:
+        return redirect(f"{url}/3.html", code=301)
     else:
-        return redirect(f"{url}/error.html", code=302)
+        return "TRY LATER", 500
 
 
 if __name__ == "__main__":
--- front/submit.js
+++ front/submit.js
@@ -1,27 +1,28 @@
-const submitButton = document.querySelector(".button--submit");
-
-
 const form = document.querySelector("form");
 
-function handleSubmit(event){
-    event.preventDefault();
-    const answer1 = document.querySelector('input[name=answer1]:checked').value;
-    const answer2 = document.querySelector('input[name=answer2]:checked').value;
-    const answer3 = document.querySelector('input[name=answer3]:checked').value;
-    const answer4 = document.querySelector('input[name=answer4]:checked').value;
-    const answer5 = document.querySelector('input[name=answer5]:checked').value;
-    const answer6 = document.querySelector('input[name=answer6]:checked').value;
-    const answer7 = document.querySelector('input[name=answer7]:checked').value;
-    const answer8 = document.querySelector('input[name=answer8]:checked').value;
-    const answer_mbti = document.querySelector('input[name=answer_mbti]').value;
-    const answers = answer1 + answer2 + answer3 + answer4 + answer5 + answer6 + answer7 + answer8 + answer_mbti;
-    // fetch('http://3.39.5.135/survey/' + answers) //in AWS
-    fetch('http://localhost:8000/survey/' + answers) // in Localhost
-    .then(function(response) {
-        console.log(JSON.stringify(response));
-        return response.json();
+function handleSubmit(event) {
+  event.preventDefault();
+  const answer1 = document.querySelector('input[name=answer1]:checked').value;
+  const answer2 = document.querySelector('input[name=answer2]:checked').value;
+  const answer3 = document.querySelector('input[name=answer3]:checked').value;
+  const answer4 = document.querySelector('input[name=answer4]:checked').value;
+  const answer5 = document.querySelector('input[name=answer5]:checked').value;
+  const answer6 = document.querySelector('input[name=answer6]:checked').value;
+  const answer7 = document.querySelector('input[name=answer7]:checked').value;
+  const answer8 = document.querySelector('input[name=answer8]:checked').value;
+  const answer_mbti = document.querySelector('input[name=answer_mbti]').value;
+  const answers = answer1 + answer2 + answer3 + answer4 + answer5 + answer6 + answer7 + answer8 + answer_mbti;
+  if (answer_mbti === "") { //Q9 문항에 답하지 않으면 동물 페이지로 이동
+    fetch('http://3.39.5.135/survey/' + answers, { method: 'POST' })
+    .then(response => {
+        if (response.redirected) {
+            window.location.href = response.url;
+        }
     })
+  } else { // Q9 문항에 답하면 (SQL) 데이터에 등록
+    fetch('http://3.39.5.135/survey/' + answers)
   }
+}
 
 
 function init(){
